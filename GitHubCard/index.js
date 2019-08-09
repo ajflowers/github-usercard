db8c74075e7213b4d9a550b966432bcf3563e46e
Angela Flowers <me@angela.flowers>
2019-08-08 21:21:56 -0400

    successfully integrated API

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,7 +24,13 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +59,115 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+
+axios.get('https://api.github.com/users/ajflowers')
+  .then((response) => {
+    console.log(response);
+    console.log(response.data);
+    // console.log(testObj);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+
+
+const cards = document.querySelector('.cards');
+// console.log(cards);
+
+const makeCard = function (user) {
+
+  //card - outer div to return
+  const card = document.createElement('div');
+  card.className = "card";
+
+  //image
+  const cardImg = document.createElement('img');
+  cardImg.setAttribute('src', user.avatar_url);
+  cardImg.setAttribute('alt', `${user.name}'s GitHub profile picture`);
+  
+  card.appendChild(cardImg);
+
+  //info div
+  const cardInfo = document.createElement('div');
+  cardInfo.className = 'card-name';
+
+  card.appendChild(cardInfo);
+
+  //name
+  const cardName = document.createElement('h3');
+  cardName.className = 'name';
+  cardName.append(user.name);
+
+  cardInfo.appendChild(cardName);
+  
+  //login
+  const cardLogin = document.createElement('p');
+  cardLogin.className = 'username';
+  cardLogin.append(user.login);
+  
+  cardInfo.appendChild(cardLogin);
+
+  //location
+  const cardLocation = document.createElement('p');
+  cardLocation.append(`Location: ${user.location}`);
+  
+  cardInfo.appendChild(cardLocation);
+
+  //profile
+  const cardProfile = document.createElement('p');
+  cardProfile.append("Profile: ");
+
+  cardInfo.appendChild(cardProfile);
+
+  //profile link - APPENDS TO PROFILE NOT INFO
+  const cardProfLink = document.createElement('a');
+  cardProfLink.append(user.html_url);
+  cardProfLink.setAttribute("href", user.html_url);
+  
+  cardProfile.appendChild(cardProfLink);
+
+  //followers
+  const cardFollowers = document.createElement('p');
+  cardFollowers.append(`Followers: ${user.followers}`);
+
+  cardInfo.appendChild(cardFollowers);
+
+  //followers
+  const cardFollowing = document.createElement('p');
+  cardFollowing.append(`Following: ${user.following}`);
+
+  cardInfo.appendChild(cardFollowing);
+
+  //bio
+  const cardBio = document.createElement('p');
+  cardBio.append(`Bio: ${user.bio}`);
+
+  cardInfo.append(cardBio);
+
+  //return card
+  return card;
+}
+
+
+
+function getCard(username) {
+  axios.get(`https://api.github.com/users/${username}`)
+  .then((response) => {
+    let newCard = makeCard(response.data);
+    cards.appendChild(newCard);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+
+getCard('ajflowers');
+
+//console.log(followersArray);
+
+followersArray.forEach(follower => {
+  getCard(follower);
+})
